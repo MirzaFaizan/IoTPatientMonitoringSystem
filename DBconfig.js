f9ab3755f52044c508d17dd453e66ec84212c003
Faizan <mirzafaizanejaz@gmail.com>
2018-12-23 12:04:36 +0500

    made production build

--- DBconfig.js
+++ DBconfig.js
@@ -1,8 +1,8 @@
 module.exports = {
 
     'secret': 'superSecret',
-    'database': 'mongodb://localhost:27017/PatientMonitoringSystem',
-     //'database': 'mongodb://faizi:faizi@cluster0-shard-00-00-aquse.mongodb.net:27017,cluster0-shard-00-01-aquse.mongodb.net:27017,cluster0-shard-00-02-aquse.mongodb.net:27017/PatientMonitoringSystem?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+    //'database': 'mongodb://localhost:27017/PatientMonitoringSystem',
+     'database': 'mongodb://faizi:faizi@cluster0-shard-00-00-aquse.mongodb.net:27017,cluster0-shard-00-01-aquse.mongodb.net:27017,cluster0-shard-00-02-aquse.mongodb.net:27017/PatientMonitoringSystem?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
 
 
 };
