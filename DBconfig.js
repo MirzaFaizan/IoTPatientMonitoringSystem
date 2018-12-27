1341cb9f5a661941cecc9a174e168e2f19a4f7ff
Faizan <mirzafaizanejaz@gmail.com>
2018-12-27 09:20:37 +0500

    added new URL db

--- DBconfig.js
+++ DBconfig.js
@@ -2,7 +2,7 @@ module.exports = {
 
     'secret': 'superSecret',
     //'database': 'mongodb://localhost:27017/PatientMonitoringSystem',
-     'database': 'mongodb://faizi:faizi@cluster0-shard-00-00-aquse.mongodb.net:27017,cluster0-shard-00-01-aquse.mongodb.net:27017,cluster0-shard-00-02-aquse.mongodb.net:27017/PatientMonitoringSystem?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+     'database': 'mongodb://tayyab:12345@cluster0-shard-00-00-h4rwz.mongodb.net:27017,cluster0-shard-00-01-h4rwz.mongodb.net:27017,cluster0-shard-00-02-h4rwz.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
 
 
 };
